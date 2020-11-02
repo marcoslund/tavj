627f866a054503cb046c29f632426f419b0db3a0
mlund <mlund@itba.edu.ar>
2020-11-01 23:45:46 -0300

    Set swat animation controller

---
+++ CS2D/Assets/Scripts/Tests/ClientCopyManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientCopyManager : MonoBehaviour
+{
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animator.SetBool("Shooting", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
