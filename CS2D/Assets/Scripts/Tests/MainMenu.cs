e39cd01ee1a5c9e1e5ef712c7a0b20a675b949e7
mlund <mlund@itba.edu.ar>
2020-11-18 20:25:11 -0300

    Add Main Menu

---
+++ CS2D/Assets/Scripts/Tests/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject mainMenu;
+    public GameObject playMenu;
+    
+    public void OpenPlayMenu()
+    {
+        title.SetActive(false);
+        mainMenu.SetActive(false);
+        playMenu.SetActive(true);   
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ CS2D/Assets/Scripts/Tests/UI/PlayMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMenu : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject mainMenu;
+    public GameObject playMenu;
+
+    public TMP_InputField playerNameInput;
+    public TMP_InputField gameIpInput;
+    
+    public void ReturnToMainMenu()
+    {
+        playMenu.SetActive(false);
+        title.SetActive(true);
+        mainMenu.SetActive(true);
+    }
+    
+    public void HostGame()
+    {
+        SavePlayerPrefs();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void JoinGame()
+    {
+        SavePlayerPrefs();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    private void SavePlayerPrefs()
+    {
+        var playerName = playerNameInput.text;
+        var ip = gameIpInput.text;
+        PlayerPrefs.SetString("PlayerName", string.IsNullOrWhiteSpace(playerName)? "Player" : playerName);
+        PlayerPrefs.SetString("IP", string.IsNullOrWhiteSpace(ip)? "127.0.0.1" : ip);
+        PlayerPrefs.Save();
+    }
+}
